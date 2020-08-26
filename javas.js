47edce09d36ce398c6f1c1d2b6afb9bef8ebf5c1
Anthony Ochoa <ochoaap@yahoo.com>
2020-08-26 02:08:35 -0700

    updating with functions

--- javas.js
+++ javas.js
@@ -1,5 +1,17 @@
-prompt("Do you have a favorite Superhero?")
 
-prompt("Do you prefer DC or Marvel?")
+function askuserName(){var userName = prompt("What is your name?");
+document.write('<h2>' + 'Hiya, ' + userName + '</h2>');}
 
-confirm("Press this button!")
+
+
+confirm("I want to know about your favorite SuperHero!")
+
+function favorite(){
+var suprhero;
+    superhero=prompt("Who is your favorite super hero?");
+    alert("Oh, they're really cool! Check out mine!")
+}
+
+askuserName();
+
+favorite()
